e942fcf6534be3c6cf6a39861853e13a9857aaa8
Diyor <darkdido00@gmail.com>
2024-03-17 20:39:14 +0500

    Initial Commit

---
+++ iOS Project IITU/iOS_Project_IITUApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Project_IITUApp.swift
+//  iOS Project IITU
+//
+//  Created by Diyor Khabibullayev on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Project_IITUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
